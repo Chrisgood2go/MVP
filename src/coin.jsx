36e585ad0ada7b43d7fe837c1ecfd3dab30f552e
Christopher Garcia <chrisgood2go@gmail.com>
2022-09-28 00:40:13 -0400

    add functionality to coins component and make axios call in index.jsx

--- src/coin.jsx
+++ src/coin.jsx
@@ -1,31 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Btn, CallSel, CallDiv, InputStake, Stakes, Tails, Heads, Circle, Column } from './style.jsx'
 
 
 
 const Coin = (props) => {
+  const[side, setSide]=useState('HEADS');
+
+  var handleChange = (e) => {
+    props.setStakes(e.target.value)
+  }
+
+  var handleSelction =(e)=>{
+    props.setCall(e.target.value)
+    setSide(e.target.value)
+  }
+
+  var handleTossBtn = (e)=>{
+    let count = 0;
+    let diff = Math.abs(100-1);
+    let random = (Math.round(Math.random()* diff +1));
+    let IntervalId = setInterval(()=>{
+      setSide(count % 2 === 0? 'HEAD': 'TAILS')
+      count++
+    },100)
+
+    setTimeout(()=>{
+      clearInterval(IntervalId)
+      props.setResult(random % 2 === 0 ? 'HEADS' : 'TAILS');
+      setSide(random % 2 === 0 ? 'HEADS' : 'TAILS')
+    },1000)
+  }
+
   return (
     <Column>
       <h1 >Chance Toss</h1>
       <p> SEE WHAT CHANCE WILL YIELD</p>
       <br />
-      <Circle>
-        <Heads>HEADS</Heads>
+      <Circle >
+        <Heads>{side}</Heads>
       </Circle>
       <br />
       <br />
       < Stakes htmlFor="stakes"> STAKES </Stakes>
-      <InputStake id="stakes" name="stakes" maxlength="280" />
+      <InputStake id="stakes" name="stakes" maxlength="280" onChange={handleChange}/>
       <br />
       <br />
       <CallDiv>
-        <CallSel>
-          <option value="0">HEADS</option>
-          <option value="1">TAILS</option>
+        <CallSel onChange = {handleSelction}>
+          <option value="HEADS">HEADS</option>
+          <option value="TAILS">TAILS</option>
         </CallSel>
         <span> </span>
-        <Btn>TOSS</Btn>
+        <Btn onClick ={handleTossBtn}>TOSS</Btn>
       </CallDiv>
     </Column>
 
--- src/index.jsx
+++ src/index.jsx
@@ -4,37 +4,58 @@ import Coin from './coin.jsx'
 import Tosses from './tosses.jsx'
 import axios from 'axios';
 import styled from 'styled-components';
-import {Row,Column} from './style.jsx'
+import { Row, Column } from './style.jsx'
 
 
 
 
 const App = () => {
-  const[tosses, setTosses]= useState('')
-  const [stakes, setStakes] =useState('');
-  const[call, setCall] =useState('HEADS');
-  const[result, setResult]=useState('');
+  const [tosses, setTosses] = useState('')
+  const [stakes, setStakes] = useState('');
+  const [call, setCall] = useState('HEADS');
+  const [result, setResult] = useState('');
 
-  React.useEffect(()=>{
-    axios.get(`http://localhost:3000/recentTosses/`)
-    .catch(err => {
-      console.log(err)
-    })
-    .then(response => {
-      setTosses(response.data)
-    })
-  },[])
 
-return (
-  <div>
-      <Coin setStakes = {setStakes} setCall = {setCall} setResult = {setResult}></Coin>
-<Tosses></Tosses>
 
-  </div>
+  React.useEffect(() => {
+    if (result !== '') {
+      axios.post(`http://localhost:3000/Toss`, {
+        call: call,
+        result: result,
+        stakes: stakes !== '' ? stakes : 'nothing entered',
+        settled: false
+      })
+        .catch(err => { console.log(err) })
+        .then(response => {
+          axios.get(`http://localhost:3000/recentTosses`)
+            .catch(err => {
+              console.log(err)
+            })
+            .then(response => {
+              setTosses(response.data)
+            })
+        })
+    } else {
+      axios.get(`http://localhost:3000/recentTosses`)
+        .catch(err => {
+          console.log(err)
+        })
+        .then(response => {
+          setTosses(response.data)
+        })
+    }
+  }, [])
+
+  return (
+    <div>
+      <Coin setStakes={setStakes} setCall={setCall} setResult={setResult}></Coin>
+      <Tosses></Tosses>
+
+    </div>
 
 
   );
 }
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App/>);
+root.render(<App />);
